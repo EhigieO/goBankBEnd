7f5486a088bb499bf520dc4e09d6c55974a92d56
EhigieO <eshioke@gmail.com>
2022-12-14 16:51:45 +0100

    Build constraints exclude all the Go files in /folder? problem

--- utils/utils.go
+++ utils/utils.go
@@ -1,23 +1,17 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"rovaBankProject/response"
 	"time"
 )
 
 func GenerateAccountNumber() string {
-	return idGen()
-}
-func GenerateTransactionId() string {
-	return idGen()
-}
-func GenerateCustomerID() string {
-	return idGen()
-}
-
-func idGen() string {
 	var letter = []rune("0123456789")
 
 	rand.Seed(time.Now().UnixNano())
@@ -27,5 +21,42 @@ func idGen() string {
 	}
 	return string(accountNumber)
 }
+func GenerateTransactionId() string {
+	return GenerateCustomerID()
+}
+
+var customerCounter int64 = 1
+
+func GenerateCustomerID() string {
+	customerCounter += 1
+	return fmt.Sprintf("%06d", customerCounter)
+}
 
 var ApplicationLog = log.New(os.Stdout, "[account-service] ", log.LstdFlags)
+
+func GenerateJSONResponse(c *gin.Context, statusCode int, message string, data map[string]interface{}) {
+	c.JSON(statusCode, response.APIResponse{
+		Status:    statusCode,
+		Message:   message,
+		Timestamp: time.Now(),
+		Data:      data,
+	})
+}
+
+func GenerateInternalServerErrorResponse(c *gin.Context, message string) {
+	c.JSON(http.StatusInternalServerError, response.APIResponse{
+		Status:    http.StatusInternalServerError,
+		Message:   message,
+		Timestamp: time.Now(),
+		Data:      gin.H{},
+	})
+}
+
+func GenerateBadRequestResponse(c *gin.Context, message string) {
+	c.JSON(http.StatusBadRequest, response.APIResponse{
+		Status:    http.StatusBadRequest,
+		Message:   message,
+		Timestamp: time.Now(),
+		Data:      gin.H{},
+	})
+}
